4503af436580fe85485923972304b5daa8fbb1c1
Marcelo Amado De León Gómez <soportevr@ragasa.com.mx>
2023-11-20 21:21:45 -0600

    Se agregan comentarios

--- insercion.py
+++ insercion.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+# en esta función se calcula la distancia euclidiana entre dos puntos
 def euclidean_distance(node1, node2):
     x1, y1 = node1[1], node1[2]
     x2, y2 = node2[1], node2[2]
@@ -10,6 +11,7 @@ with open('6nodes.txt', 'r') as file:
 
 start_idx = lines.index('NODE_COORD\n') + 1
 
+# en este apartado se establecen los nodos y sus coordenadas
 nodes = []
 for line in lines[start_idx:]:
     data = line.strip().split()
@@ -17,6 +19,7 @@ for line in lines[start_idx:]:
         node = [int(data[0]), int(data[1]), int(data[2])]
         nodes.append(node)
 
+# en esta función sucede el proceso principal del algoritmo
 def cheapest_insertion(nodes):
     n = len(nodes)
     route = [nodes[0]]
@@ -26,6 +29,7 @@ def cheapest_insertion(nodes):
         min_distance = np.inf
         min_index = -1
 
+        # se comprueba si la distancia actual es menor que la más corta para quedarnos con esa como la más corta
         for i, node in enumerate(remaining_nodes):
             for j in range(len(route)):
                 dist_increase = euclidean_distance(route[j], node) + euclidean_distance(node, route[(j+1) % len(route)]) - euclidean_distance(route[j], route[(j+1) % len(route)])
@@ -35,6 +39,7 @@ def cheapest_insertion(nodes):
                     min_index = i
                     insertion_point = j + 1
 
+        # se inserta el nodo con la distancia menor a la ruta
         route.insert(insertion_point, remaining_nodes[min_index])
         del remaining_nodes[min_index]
 
@@ -42,6 +47,7 @@ def cheapest_insertion(nodes):
 
 optimal_route = cheapest_insertion(nodes)
 
+# se calcula la distancia total en la ruta
 total_distance = sum(euclidean_distance(optimal_route[i], optimal_route[(i+1) % len(optimal_route)]) for i in range(len(optimal_route)))
 
 print("Ruta óptima:")
